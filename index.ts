ba67d29b4dd2eb91939a4eb459eb62e038612fab
Antoine Kahlouche <antoine@kahlouche.fr>
2025-11-08 21:19:52 +0100

    Add Photobooth app with Bun server and Dropzone UI

---
+++ index.ts
@@ -0,0 +1,18 @@
+Bun.serve({
+    port: 80,
+    async fetch(req) {
+        const path = new URL(req.url).pathname;
+
+        if (path === "/") return new Response(Bun.file("./index.html"));
+
+        if (req.method === "POST" && path === "/upload") {
+            const formdata = await req.formData();
+            const file = formdata.get("file");
+            if (!file) return new Response("No file provided", { status: 400 });
+            await Bun.write("./data/" + Date.now() + "-" + file.name, file);
+            return new Response("Success");
+        }
+
+        return new Response("Page not found", { status: 404 });
+    },
+});
